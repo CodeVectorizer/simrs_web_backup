f10584f6f00dda00a01918b614ba1b6e54317045
Rapiii <72059714+Hamidrafi712@users.noreply.github.com>
2022-06-14 12:55:06 +0700

    upload models post, spesialis dan video

---
+++ app/Models/Post.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $fillable = ['category_id', 'title', 'content', 'image'];
+
+    /**
+     * Get all of the Category for the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+
+    /**
+     * Get the category that owns the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
